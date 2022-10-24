2b18ec1ad9ff5080f9640b00f2399e2ebcdd7377
kimjiyun725 <kimjiyun725@gmail.com>
2022-10-24 11:04:15 -0400

    Made it more concise

--- m_1328.py
+++ m_1328.py
@@ -2,19 +2,15 @@ def breakPalindrome(self, palindrome: str) -> str:
     if len(palindrome) == 1:
         return ""
 
-    length = len(palindrome) // 2
-    char_vals = {i: ord(palindrome[i]) for i in range(length)}
     smallest_idx = -1
-    for i in range(length):
-        if ord('a') < char_vals[i] < ord('z')+1:
+    for i in range(len(palindrome) // 2):
+        if 'a' < palindrome[i] <= 'z':
             smallest_idx = i
             break
             
     if smallest_idx == -1:
-        return ''.join([palindrome[0:len(palindrome)-1], 'b'])
-    
+        return ''.join([palindrome[:-1], 'b']) 
     return ''.join([palindrome[:smallest_idx], 'a', palindrome[smallest_idx+1:]])
-
 
 # https://leetcode.com/problems/break-a-palindrome/
 # Time Complextiy: O(n) 'palindrome' is length of n and all for-loops iterates
